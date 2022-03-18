85341fae69829793694469975286a1d274446b01
tienloctonypham <tienloctonypham@gmail.com>
2022-03-18 17:54:51 -0400

    updates

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('src'));
 
-let port = 3001;
+let port = process.env.PORT || parseInt(process.argv.pop()) || 3001
 
 app.listen(port, () => console.log('Example app listening on port ' + port + '!'));
